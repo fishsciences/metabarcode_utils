4990eaa4551715642afb5ba2086121fe8a080e66
Matt Espe <mespe@ucdavis.edu>
2021-10-29 16:22:33 -0600

    Fixing typos.

--- R/batch.R
+++ R/batch.R
@@ -16,20 +16,66 @@
 ##'     csv_to_fasta
 ##' @author Matt Espe
 ##' @export
-batch_mito_to_csv = function(file_dir,
+batch_mitofish_to_csv = function(file_dir,
                              out_dir, ...)
 {
-    batch_generic(file_dir, out_dir, "\\.xlsx$", mito_to_csv, ...)
+    batch_generic(file_dir, out_dir, "\\.xlsx$", mitofish_to_csv, ...)
 }
 
-##' @rdname batch_mito_to_csv
+##' @rdname batch_mitofish_to_csv
 ##' @export
 batch_csv_to_fasta = function(csv_dir,
                               out_dir, ...)
 {
     batch_generic(csv_dir, out_dir, "\\.csv$", csv_to_fasta, ...)
 }
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title
+##' @param fasta_dir directory containing fasta files to combine
+##' @param fasta_pattern the pattern to match for fasta files
+##'     (e.g. "NoHit")
+##' @param max_records optional, maximum number of records for each
+##'     file. The total lines in each file will be twice this number
+##'     (since records span two lines). If not provided, all files
+##'     will be written to a single fasta file
+##' @param new_names character, the base of the name for the new
+##'     combined files
+##' @param out_dir optional, if not provided combined files will be
+##'     written into the same directory as input files.
+##' @param fasta_files vector of files to combine
+##' @return file names of new combined files
+##' @author Matt Espe
+##' @export
+combine_fasta = function(fasta_dir, 
+                         fasta_pattern,
+                         max_records,
+                         new_names = paste0("combined_", fasta_pattern, "_"),
+                         out_dir,
+                         fasta_files = list.files(fasta_dir, fasta_pattern, full.names = TRUE))
+{
+    
+    ll = do.call(c, lapply(readLines(fasta_files)))
 
+    i = if(!missing(max_records)){
+            seq(length(ll)) %/% (max_records * 2) # each record is 2 lines
+        } else
+            rep(1, length(ll)) #hacky way to put in list
+    
+    ll = split(ll, i)
+
+    if(missing(out_dir))
+        out_dir = fasta_dir
+
+    new_names = file.path(out_dir, paste0(new_names, seq(along = ll)))
+    
+    mapply(writeLines, text = ll, con = new_names)
+
+    return(new_names)
+}
+
+                         
 
 ##' @param file_dir the directory to look for raw NCBI output files
 ##' @param files vector, the full file path to the raw NCBI files
